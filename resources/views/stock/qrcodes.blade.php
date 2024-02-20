5df4abc70b53050e4175c51ec367d9cc5ce51c1b
crmhawkins <128600888+crmhawkins@users.noreply.github.com>
2024-02-20 09:32:31 +0100

    qrs

--- resources/views/stock/qrcodes.blade.php
+++ resources/views/stock/qrcodes.blade.php
@@ -4,25 +4,42 @@
 <head>
     <title>Códigos QR para asignación de stock (Productos)</title>
     <style>
+        body {
+            font-family: Arial, sans-serif;
+        }
+        .container {
+            text-align: center;
+            margin: 0 auto;
+            padding: 20px;
+        }
         .qr-code {
-            display: inline-block;
+            display: inline-flex;
             margin: 10px;
+            justify-content: center;
+            align-items: center;
+            width: 150px; /* Ajusta este valor para cambiar el tamaño de los QR */
+            height: 150px; /* Ajusta este valor para cambiar el tamaño de los QR */
+        }
+        img {
+            max-width: 100%;
+            max-height: 100%;
         }
         @page {
-            margin-left: 4% !important;
-            margin-right: 3% !important;
-            margin-bottom: 0 !important;
-            padding: 0 !important;
+            margin: 10mm;
+        }
+        h2 {
+            margin-bottom: 40px;
         }
     </style>
 </head>
 
 <body>
-    <div>
-        <h2 style="text-align: center">Códigos QR para asignación de stock (Productos)</h2>
-        <br>
+    <div class="container">
+        <h2>Códigos QR para asignación de stock (Productos)</h2>
         @foreach ($qrcodes as $qr)
-            <img class="qr-code" src="data:image/png;base64, {!! base64_encode($qr) !!}">
+            <div class="qr-code">
+                <img src="data:image/png;base64, {!! base64_encode($qr) !!}">
+            </div>
         @endforeach
     </div>
 </body>
