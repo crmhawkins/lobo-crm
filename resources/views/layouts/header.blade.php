f1d3d46d3d47cc0e20f2fea2a33a1dfab7fd5509
crmhawkins <128600888+crmhawkins@users.noreply.github.com>
2024-02-20 17:13:18 +0100

    version 1.5.8

--- resources/views/layouts/header.blade.php
+++ resources/views/layouts/header.blade.php
@@ -1,7 +1,7 @@
     <div class="row header_row" style="align-content: center !important">
         <div class="col-4 col-md-6 header_logo">
             <img src="{{ asset('assets/images/logo_la_fabrica.png') }}" style="max-width: 10%" class="logo" onclick='window.location="/home"'>
-            <span style="font-size: 10px; color: gray; position: absolute; top: 0; right: 0;">v1.0.0</span>
+            <span style="font-size: 10px; color: gray; position: absolute; top: 0; right: 0;">v1.5.8</span>
         </div>
         <div class="col-4 col-md-4 header_user">
             <div class="row user" style="align-content: center">
