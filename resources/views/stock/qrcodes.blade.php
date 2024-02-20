ca94443c8ac865f2961916ddfdb4d89110895db2
crmhawkins <128600888+crmhawkins@users.noreply.github.com>
2024-02-20 16:23:36 +0100

    test 7

--- resources/views/stock/qrcodes.blade.php
+++ resources/views/stock/qrcodes.blade.php
@@ -6,33 +6,26 @@
     <style>
         body {
             font-family: Arial, sans-serif;
-            margin: 0;
-            padding: 0;
         }
         .container {
             text-align: center;
-            margin: 20px auto; /* Ajusta este valor para el margen exterior */
+            margin: 0 auto;
             padding: 20px;
-            max-width: 800px; /* Ajusta este valor según tus necesidades */
-            display: flex;
-            flex-wrap: wrap;
-            justify-content: space-between;
         }
         .qr-code {
+            display: inline-flex;
             margin: 10px;
-            width: calc(50% - 20px); /* Ajusta este valor para cambiar el espacio entre los códigos QR */
-            max-width: 180px; /* Ajusta este valor para limitar el ancho máximo de los códigos QR */
-            height: auto; /* Para mantener la proporción */
+            justify-content: center;
+            align-items: center;
+            width: 150px; /* Ajusta este valor para cambiar el tamaño de los QR */
+            height: 150px; /* Ajusta este valor para cambiar el tamaño de los QR */
         }
         img {
             max-width: 100%;
-            height: auto;
-            display: block;
-            margin: 0 auto; /* Centra la imagen horizontalmente */
+            max-height: 100%;
         }
         @page {
             margin: 10mm;
-            size: A4 landscape; /* Cambiar a A4 landscape para orientación horizontal */
         }
         h2 {
             margin-bottom: 40px;
@@ -52,4 +45,3 @@
 </body>
 
 </html>
-
