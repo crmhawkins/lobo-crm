89831e98717b70d01e65aafc70ea40e5457986df
crmhawkins <128600888+crmhawkins@users.noreply.github.com>
2024-03-18 11:17:17 +0100

    test

--- app/Http/Livewire/Stock/IndexComponent.php
+++ app/Http/Livewire/Stock/IndexComponent.php
@@ -55,7 +55,7 @@ class IndexComponent extends Component
         $codigo = Stock::where('id', $stock_id)->orderBy('created_at', 'desc')->first()->qr_id;
         if(isset($codigo)){
         $Qrcode= QrCode::errorCorrection('H')->format('png')->eye('circle')->size('300')->merge('/public/assets/images/lobo-qr.png')->errorCorrection('H')->generate($codigo);
-
+            dd($Qrcode);
         $pdf = PDF::loadView('stock.qrindividual', compact('Qrcode'))->setPaper('a4');
 
         return $pdf->stream('qrindividual.pdf');
