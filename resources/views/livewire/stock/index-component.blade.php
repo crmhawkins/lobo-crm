6ed2b5f5366b7f2ecd957fe504e4b38ade90a07a
crmhawkins <128600888+crmhawkins@users.noreply.github.com>
2024-04-03 17:52:34 +0200

    correccion

--- resources/views/livewire/stock/index-component.blade.php
+++ resources/views/livewire/stock/index-component.blade.php
@@ -227,12 +227,12 @@
         }
     </script>
 
-<script src="../assets/js/jquery.slimscroll.js"></script>
-<link href="https://cdn.datatables.net/v/bs5/jq-3.7.0/jszip-3.10.1/dt-2.0.3/b-3.0.1/b-colvis-3.0.1/b-html5-3.0.1/b-print-3.0.1/r-3.0.1/datatables.min.css" rel="stylesheet">
+<link href="https://cdn.datatables.net/v/bs4/jszip-3.10.1/dt-2.0.3/b-3.0.1/b-colvis-3.0.1/b-html5-3.0.1/r-3.0.1/datatables.min.css" rel="stylesheet">
+
 <script src="https://cdnjs.cloudflare.com/ajax/libs/pdfmake/0.2.7/pdfmake.min.js"></script>
 <script src="https://cdnjs.cloudflare.com/ajax/libs/pdfmake/0.2.7/vfs_fonts.js"></script>
-<script src="https://cdn.datatables.net/v/bs5/jq-3.7.0/jszip-3.10.1/dt-2.0.3/b-3.0.1/b-colvis-3.0.1/b-html5-3.0.1/b-print-3.0.1/r-3.0.1/datatables.min.js"></script>
-<!-- Responsive examples -->
-<script src="../assets/pages/datatables.init.js"></script>
+<script src="https://cdn.datatables.net/v/bs4/jszip-3.10.1/dt-2.0.3/b-3.0.1/b-colvis-3.0.1/b-html5-3.0.1/r-3.0.1/datatables.min.js"></script>
+
+
 
 @endsection
