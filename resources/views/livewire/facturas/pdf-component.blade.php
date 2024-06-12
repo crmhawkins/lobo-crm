6380fb0c0b846454e9a7cf0ae5a2b53d1bd5f52f
jgonmor <antonio@hawkins.es>
2024-06-12 14:17:54 +0200

    fix factura rectificativa

--- resources/views/livewire/facturas/pdf-component.blade.php
+++ resources/views/livewire/facturas/pdf-component.blade.php
@@ -64,8 +64,8 @@
                 ALGECIRAS (CÁDIZ) 11205, España
             </td>
             <td width="20%">&nbsp;</td>
-            <td class="bold" width="40%" style="text-align: right !important">
-                <h1 style="display: inline; color:#0196eb; font-weight:bolder ;">FACTURA</h1><br>
+            <td class="bold"  @if($factura->tipo == 2) width="60%"  @else width="40%" @endif style="text-align: right !important">
+                <h1 style="display: inline; color:#0196eb; font-weight:bolder ;">FACTURA @if($factura->tipo == 2) RECTIFICATIVA @endif</h1><br>
                 
                 <span style="font-size: 80%"><span style="font-weight: bold;">#{{$factura->numero_factura}}</span><br>
                 @if(isset($pedido) )
