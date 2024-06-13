32d4955cdb429e276d492362804bc48ebbf60783
jgonmor <antonio@hawkins.es>
2024-06-13 14:15:33 +0200

    change enviarwhatsapp

--- app/Helpers/helpers.php
+++ app/Helpers/helpers.php
@@ -1,6 +1,6 @@
 <?php
 if (!function_exists('enviarMensajeWhatsApp')) {
-    function enviarMensajeWhatsApp($template, $data, $telefono, $idioma = 'es') {
+    function enviarMensajeWhatsApp($template, $data, $buttondata, $telefono, $idioma = 'es') {
         $token = env('TOKEN_WHATSAPP', 'valorPorDefecto');
 
         if (count($data) > 0) {
@@ -20,6 +20,16 @@ if (!function_exists('enviarMensajeWhatsApp')) {
                     ],
                 ],
             ];
+
+            if(count($buttondata) > 0){
+                $mensajePersonalizado['template']['components'][] = [
+                    "type" => "button",
+                    "sub_type" => "url",
+                    "index" => 0,
+                    "parameters" => $buttondata
+                ];
+            }
+
         } else {
             $mensajePersonalizado = [
                 "messaging_product" => "whatsapp",
@@ -31,6 +41,15 @@ if (!function_exists('enviarMensajeWhatsApp')) {
                     "language" => ["code" => $idioma],
                 ],
             ];
+
+            if(count($buttondata) > 0){
+                $mensajePersonalizado['template']['components'][] = [
+                    "type" => "button",
+                    "sub_type" => "url",
+                    "index" => 0,
+                    "parameters" => $buttondata
+                ];
+            }
         }
 
         $urlMensajes = 'https://graph.facebook.com/v19.0/367491926438581/messages';
