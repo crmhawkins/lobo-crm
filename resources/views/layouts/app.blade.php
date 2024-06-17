473f0a894ad04df4dc5050df0049d07859fc9a1d
jgonmor <antonio@hawkins.es>
2024-06-17 13:50:00 +0200

    set Loader to all pages

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -74,6 +74,13 @@
         $user_rol = $user->role;
     @endphp
     <div id="app">
+        <div id="loadingOverlay" style="display: block; position: fixed; width: 100%; height: 100%; top: 0; left: 0; right: 0; bottom: 0; background-color: rgba(255,255,255,0.5); z-index: 50000; cursor: pointer;">
+            <div style="position: absolute; top: 50%; left: 50%; transform: translate(-50%, -50%);">
+                <div class="spinner-border text-black" role="status">
+                    <span class="sr-only">Cargando...</span>
+                </div>
+            </div>
+        </div>
         <style>
             /* .sidebar {
                 width: 15%;
@@ -139,6 +146,18 @@
 
     <!-- App js -->
     <script src="/assets/js/app.js"></script>
+    <script>
+
+        document.addEventListener("DOMContentLoaded", function() {
+                    var loader = document.getElementById('loadingOverlay');
+                    if (loader) {
+                        loader.style.display = 'none';
+                        setTimeout(() => {
+                        }, 1500);
+                    }
+                });
+
+    </script>
 
     @livewireScripts
     @yield('scripts')
