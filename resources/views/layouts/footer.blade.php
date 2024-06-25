749b572804807b7feacac4366d7dc5e68916a76b
jgonmor <antonio@hawkins.es>
2024-06-25 08:48:47 +0200

    Sandra puede acceder al almacen y crear facturas

--- resources/views/layouts/footer.blade.php
+++ resources/views/layouts/footer.blade.php
@@ -1563,6 +1563,7 @@
                     <span>VOLVER</span>
                 </button>
             </div>
+            
             <div class="menu_footer_new">
                 <div class="exit-button-col">
                     <a class="footer-button" href="{{ route('productos.index') }}">
@@ -1615,6 +1616,23 @@
                             <path d="M13 17l2 0"></path>
                         </svg>
                         <span>FACTURAS</span>
+                    </a>
+                </div>
+                <div class="exit-button-col">
+                    <a class="footer-button" href="{{ route('almacen.index') }}">
+                        <svg xmlns="http://www.w3.org/2000/svg" class="icon icon-tabler icon-tabler-brand-codesandbox"
+                            width="44" height="44" viewBox="0 0 24 24" stroke-width="1.5" stroke="#35a8e0"
+                            fill="none" stroke-linecap="round" stroke-linejoin="round">
+                            <path stroke="none" d="M0 0h24v24H0z" fill="none" />
+                            <path d="M20 7.5v9l-4 2.25l-4 2.25l-4 -2.25l-4 -2.25v-9l4 -2.25l4 -2.25l4 2.25z" />
+                            <path d="M12 12l4 -2.25l4 -2.25" />
+                            <line x1="12" y1="12" x2="12" y2="21" />
+                            <path d="M12 12l-4 -2.25l-4 -2.25" />
+                            <path d="M20 12l-4 2v4.75" />
+                            <path d="M4 12l4 2l0 4.75" />
+                            <path d="M8 5.25l4 2.25l4 -2.25" />
+                        </svg>
+                        <span>ALMACÉN</span>
                     </a>
                 </div>
                 <div class="exit-button-col">
