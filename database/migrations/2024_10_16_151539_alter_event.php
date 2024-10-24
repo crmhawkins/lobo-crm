3c319a77f2bdd810494f51dd747d2018d2f42733
jgonmor <antonio@hawkins.es>
2024-10-24 11:44:20 +0200

    migraciones alertas y eventos

---
+++ database/migrations/2024_10_16_151539_alter_event.php
@@ -0,0 +1,33 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        //change column title to text
+        Schema::table('event', function (Blueprint $table) {
+            $table->text('title')->change();
+            //change column location to text
+            $table->text('location')->change();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        //
+    }
+};
---
+++ database/migrations/2024_10_16_154949_alter_alertas.php
@@ -0,0 +1,31 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        //add column imagen to alertas
+        Schema::table('alertas', function (Blueprint $table) {
+            $table->string('imagen')->nullable();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        //
+    }
+};
