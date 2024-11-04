35cebbeeb97d0b2094e64f34ec1f635ac3d6e0ff
jgonmor <antonio@hawkins.es>
2024-11-04 10:18:23 +0100

    PRODUCTOS MARKETING EN MENU

--- resources/views/layouts/footer.blade.php
+++ resources/views/layouts/footer.blade.php
@@ -85,6 +85,12 @@
                     </a>
                 </div>
                 <div class="exit-button-col">
+                    <a class="footer-button" href="{{ route('productosmarketing.index') }}">
+                        <i class="fa-solid fa-wine-bottle"></i>
+                        <span>PRODUCTOS MKT</span>
+                    </a>
+                </div>
+                <div class="exit-button-col">
                     <a class="footer-button" href="{{ route('pedidos.create') }}">
                         <svg xmlns="http://www.w3.org/2000/svg" class="icon icon-tabler icon-tabler-circle-plus" width="44"
                             height="44" viewBox="0 0 24 24" stroke-width="1.5" stroke="#35a8e0" fill="none"
@@ -552,6 +558,12 @@
                     </a>
                 </div>
                 <div class="exit-button-col">
+                    <a class="footer-button" href="{{ route('productosmarketing.index') }}">
+                        <i class="fa-solid fa-wine-bottle"></i>
+                        <span>PRODUCTOS MKT</span>
+                    </a>
+                </div>
+                <div class="exit-button-col">
                     <a class="footer-button" href="{{ route('pedidos.create') }}">
                         <svg xmlns="http://www.w3.org/2000/svg" class="icon icon-tabler icon-tabler-circle-plus"
                             width="44" height="44" viewBox="0 0 24 24" stroke-width="1.5" stroke="#35a8e0"
@@ -881,6 +893,12 @@
                             <path d="M8 5.25l4 2.25l4 -2.25" />
                         </svg>
                         <span>PRODUCTOS</span>
+                    </a>
+                </div>
+                <div class="exit-button-col">
+                    <a class="footer-button" href="{{ route('productosmarketing.index') }}">
+                        <i class="fa-solid fa-wine-bottle"></i>
+                        <span>PRODUCTOS MKT</span>
                     </a>
                 </div>
                 <div class="exit-button-col">
@@ -1537,6 +1555,12 @@
                     </a>
                 </div>
                 <div class="exit-button-col">
+                    <a class="footer-button" href="{{ route('productosmarketing.index') }}">
+                        <i class="fa-solid fa-wine-bottle"></i>
+                        <span>PRODUCTOS MKT</span>
+                    </a>
+                </div>
+                <div class="exit-button-col">
                     <a class="footer-button" href="{{ route('admin.incidencias.index') }}">
                         <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 384 512" width="44"
                         height="44" fill="#35a8e0"><!--!Font Awesome Free 6.6.0 by @fontawesome - https://fontawesome.com License - https://fontawesome.com/license/free Copyright 2024 Fonticons, Inc.--><path d="M112.5 301.4c0-73.8 105.1-122.5 105.1-203 0-47.1-34-88-39.1-90.4 .4 3.3 .6 6.7 .6 10C179.1 110.1 32 171.9 32 286.6c0 49.8 32.2 79.2 66.5 108.3 65.1 46.7 78.1 71.4 78.1 86.6 0 10.1-4.8 17-4.8 22.3 13.1-16.7 17.4-31.9 17.5-46.4 0-29.6-21.7-56.3-44.2-86.5-16-22.3-32.6-42.6-32.6-69.5zm205.3-39c-12.1-66.8-78-124.4-94.7-130.9l4 7.2c2.4 5.1 3.4 10.9 3.4 17.1 0 44.7-54.2 111.2-56.6 116.7-2.2 5.1-3.2 10.5-3.2 15.8 0 20.1 15.2 42.1 17.9 42.1 2.4 0 56.6-55.4 58.1-87.7 6.4 11.7 9.1 22.6 9.1 33.4 0 41.2-41.8 96.9-41.8 96.9 0 11.6 31.9 53.2 35.5 53.2 1 0 2.2-1.4 3.2-2.4 37.9-39.3 67.3-85 67.3-136.8 0-8-.7-16.2-2.2-24.6z"/></svg>
@@ -1781,6 +1805,12 @@
                                 stroke-width="0" fill="currentColor" />
                         </svg>
                         <span>PRODUCTOS</span>
+                    </a>
+                </div>
+                <div class="exit-button-col">
+                    <a class="footer-button" href="{{ route('productosmarketing.index') }}">
+                        <i class="fa-solid fa-wine-bottle"></i>
+                        <span>PRODUCTOS MKT</span>
                     </a>
                 </div>
                 <div class="exit-button-col">
@@ -2142,6 +2172,12 @@
                             stroke-width="0" fill="currentColor" />
                     </svg>
                     <span>PRODUCTOS</span>
+                </a>
+            </div>
+            <div class="exit-button-col">
+                <a class="footer-button" href="{{ route('productosmarketing.index') }}">
+                    <i class="fa-solid fa-wine-bottle"></i>
+                    <span>PRODUCTOS MKT</span>
                 </a>
             </div>
             <div class="exit-button-col">
